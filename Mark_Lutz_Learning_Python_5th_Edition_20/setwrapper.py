7af1ab6face87d6db6993631400619dba61e8278
G.Tsokos <grtsokos@gmail.com>
2023-01-25 12:41:57 +0200

    -

--- Mark_Lutz_Learning_Python_5th_Edition_20/setwrapper.py
+++ Mark_Lutz_Learning_Python_5th_Edition_20/setwrapper.py
@@ -1,8 +1,10 @@
 
 class Set:
   def __init__(self, value = []):				 # Constructor
+    print("__init__(start): value="+str(value))
     self.data = []						 # Manages a list
     self.concat(value)
+    print("__init__(end): data="+str(self.data)); print("")
 
   def intersect(self, other):					 # other is any sequence
     res = []							 # self is the subject
@@ -12,13 +14,16 @@ class Set:
     return Set(res)						 # Return a new Set
 
   def union(self, other):					 # other is any sequence
+    print("Union:(start) self.data="+str(self.data)+", other="+str(other))
     res = self.data[:]						 # Copy of my list
     for x in other:						 # Add items in other
       if not x in res:
         res.append(x)
+    print("Union:(end) res="+str(res))
     return Set(res)
 
   def concat(self, value):					 # value: list, Set...
+    print("concat: "+str(value))
     for x in value:						 # Removes duplicates
       if not x in self.data:
         self.data.append(x)
@@ -31,6 +36,9 @@ class Set:
   def __iter__(self): return iter(self.data)			 # for x in self,...
 
 if __name__ == '__main__':					 # from setwrapper import Set
+  print("__main__: ")
   x = Set([1, 3, 5, 7])
+  print("__main__: "+str(x))
   print(x.union(Set([1, 4, 7])))				 # prints Set:[1, 3, 5, 7, 4]
-  print(x | Set([1, 4, 6]))					 # prints Set:[1, 3, 5, 7, 4, 6]
+  print("__main__: end")
+#  print(x | Set([1, 4, 6]))					 # prints Set:[1, 3, 5, 7, 4, 6]
