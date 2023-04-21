76eb73642209a9737d4ff5c38601065aa4d1f3f0
G.Tsokos <grtsokos@gmail.com>
2023-04-21 11:01:26 +0300

    Deploying a Configurable Web Server

--- terraform/Brikman/main.tf
+++ terraform/Brikman/main.tf
@@ -2,6 +2,12 @@ provider "aws" {
   region = "eu-west-3"
 }
 
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type = number
+  default = 8080
+}
+
 resource "aws_instance" "example" {
   ami                    = "ami-05e8e219ac7e82eba"
   instance_type          = "t2.micro"
@@ -9,7 +15,7 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
   user_data_replace_on_change = true
   tags = {
@@ -20,8 +26,8 @@ resource "aws_instance" "example" {
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
   ingress {
-    from_port = 8080
-    to_port = 8080
+    from_port = var.server_port
+    to_port = var.server_port
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
