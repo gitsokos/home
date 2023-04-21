cb4f55750c3de852fbf28ff4be363f4f826744ee
G.Tsokos <grtsokos@gmail.com>
2023-04-21 11:35:53 +0300

    Deploying a Cluster of Web Servers

--- terraform/Brikman/main.tf
+++ terraform/Brikman/main.tf
@@ -8,18 +8,58 @@ variable "server_port" {
   default = 8080
 }
 
-resource "aws_instance" "example" {
-  ami                    = "ami-05e8e219ac7e82eba"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+#resource "aws_instance" "example" {
+#  ami                    = "ami-05e8e219ac7e82eba"
+#  instance_type          = "t2.micro"
+#  vpc_security_group_ids = [aws_security_group.instance.id]
+#  user_data = <<-EOF
+#              #!/bin/bash
+#              echo "Hello, World" > index.html
+#              nohup busybox httpd -f -p ${var.server_port} &
+#              EOF
+#  user_data_replace_on_change = true
+#  tags = {
+#    Name = "terraform-example"
+#  }
+#}
+
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-05e8e219ac7e82eba"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
-  user_data_replace_on_change = true
-  tags = {
-    Name = "terraform-example"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier = data.aws_subnets.default.ids
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
 
@@ -39,7 +79,7 @@ resource "aws_security_group" "instance" {
   }
 }
 
-output "public_ip" {
-  value = aws_instance.example.public_ip
-  description = "The public IP address of the web server"
-}
+#output "public_ip" {
+#  value = aws_instance.example.public_ip
+#  description = "The public IP address of the web server"
+#}
