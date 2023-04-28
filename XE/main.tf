a75fad06c4f3ceffb891928fbf352dc9ca130697
G.Tsokos <grtsokos@gmail.com>
2023-04-28 15:01:20 +0300

    -

--- XE/main.tf
+++ XE/main.tf
@@ -1,6 +1,124 @@
-provider "aws" {
-  region = "eu-west-3"
+variable "instance_name" {
+  type          = string
+  default       = "main_instance"
 }
+
+variable "type" {
+  type          = string
+  default       = "t2.large"
+}
+
+#resource "aws_instance" "node" {
+#  ami           = "ami-0c6ebbd55ab05f070"
+#  instance_type = var.type
+#  tags = {
+#    Name = var.instance_name
+#  }
+#  key_name = "ec2_rsa"
+#  vpc_security_group_ids = [aws_security_group.main.id]
+#}
+
+#resource "aws_key_pair" "deployer" {
+#  key_name   = "ec2_rsa"
+#  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDUYjxF3kIZB7OeYAgRPEYCUvcTwzsEa1BsIk/odnCvphRVdhwEWdtmVuNPe4aoWCFPgQ/UB8AFIfbyunzVon088wdqmPLtA5pKGPC2Gn6kSxq5yK4+jhNVKT+qcHnS6FeTdCGDKZehoeTq6usE9FFI+56nfgvYXBJ2DjWvl7kn/fsbrU5Q30JAbDioDY9QSrPgJxGBJC62MDH3ryckqBDNwsE3O988bA85Zl3oGlYOsMis0JZIxvJJ1OX9+iEtxJjwT2FaWV/B0hl2ZhcBUry25L/nVWiQHWI6NH8o0Fj9qCFOiiTWFrri62J9YrDNS1eSv+kI9Y4X0peDeinuV2zq1Dmdyxwy+dOCHVcj7jEmb+WR1ALgipsHN27PiRMnnBiVcyQtNa/YcNbxgEIz3wYTmYFQI8EF2IjEBb0CmuoB9b5iSB8fv1Z/ebSIGqSh3slKJ5vV1EgU2bUocP3DC6tCYlIBoqolJ9IwgxholwyE+Bv4RMger9R5R6aN9P39hC0= george@experimental"
+
+#}
+
+#output "instance_nodes" {
+#  value = [aws_instance.node.*.public_dns, aws_instance.node.*.tags.Name]
+#}
+
+////////////////////////////////////////////////
+
+/*
+locals {
+  sshPrivateKey = "/home/DMALEAS/AWS_XA/a.pem"
+}
+*/
+
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # "-1" stands for all protocols
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_iam_role" "ec2_sqs_role" {
+  name = "ec2-sqs-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "sqs_full_access" {
+  name        = "sqs-full-access"
+  description = "Grant full access to SQS"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sqs:*"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "sqs_full_access" {
+  policy_arn = aws_iam_policy.sqs_full_access.arn
+  role       = aws_iam_role.ec2_sqs_role.name
+}
+
+
+resource "aws_instance" "redrive" {
+  ami           = "ami-0b7fd829e7758b06d" # Amazon Linux 2 AMI
+  instance_type = "t2.micro"
+
+  key_name             = "awsredrive"
+  iam_instance_profile = aws_iam_instance_profile.ec2_sqs_role.name
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  tags = {
+    Name = "awsredrive"
+  }
+
+  user_data = file("awsredrive.sh")
+
+}
+
+resource "aws_iam_instance_profile" "ec2_sqs_role" {
+  name = "ec2-sqs-role"
+  role = aws_iam_role.ec2_sqs_role.name
+}
+output "instance_public_ip" {
+  value = aws_instance.redrive.public_ip
+}
+
+//------------------------------------------------------------------------------------------
 
 resource "aws_sns_topic" "example_topic" {
   name            = "example-topic"
@@ -30,7 +148,7 @@ resource "aws_sqs_queue" "example_queue" {
   depends_on = [
     aws_iam_policy_attachment.example_attachment
   ]
-    
+
 }
 
 
@@ -58,9 +176,9 @@ resource "aws_sqs_queue_policy" "example_queue_policy" {
         Principal = "*"
         Action = [
           "sqs:SendMessage",
-          "sqs:CreateQueue",	#
-          "sqs:GetQueueUrl",	#
-          "sqs:ListQueues"	#
+          "sqs:CreateQueue",    #
+          "sqs:GetQueueUrl",    #
+          "sqs:ListQueues"      #
         ]
         Resource = aws_sqs_queue.example_queue.arn
         Condition = {
@@ -95,5 +213,6 @@ resource "aws_iam_policy" "example_policy" {
 resource "aws_iam_policy_attachment" "example_attachment" {
   name       = "sqs-create-attachment"
   policy_arn = aws_iam_policy.example_policy.arn
-  users      = ["iam-george"]
+  users      = ["terraform"]
+
 }
