c4c597ee81fa2712fb1c67ae21eb8fa10c7a030c
G.Tsokos <grtsokos@gmail.com>
2023-05-01 09:47:01 +0300

    update config.json

--- XE/awsredrive.sh
+++ XE/awsredrive.sh
@@ -17,6 +17,31 @@ sudo yum update -y
 
               [Install]
               WantedBy=multi-user.target" > /etc/systemd/system/awsredrive.service
+
+echo "
+[
+  {
+    "Alias": "#1",
+    "QueueUrl": "https://sqs.eu-central-1.amazonaws.com/107824434974/sqs-q1",
+/*    "RedriveUrl": "http://nohost.com/", */
+    "RedriveScrip": "/opt/redrivescript"
+    "Region": "eu-central-1",
+    "Active": true,
+    "Timeout": 10000,
+    "UseGET": true,
+    "ServiceUrl":  "https://www.google.com"
+  }
+]
+" > /opt/awsredrive/config.json
+
+echo "echo \"aline\" >> /opt/messages" > /opt/redrivescript
+sudo chown ec2-user:ec2-user /opt/redrivescript
+sudo chmod 755 /opt/redrivescript
+
+sudo touch /opt/messages
+sudo chown ec2-user:ec2-user /opt/messages
+sudo chmod 666 /opt/messages
+
 	     chmod 700 /opt/awsredrive/AWSRedrive.console
               sudo systemctl daemon-reload
               sudo systemctl enable awsredrive.service
